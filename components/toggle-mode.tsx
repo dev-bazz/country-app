262cff9c30373c39a64cd1f9a6707240b65209a7
Clement Bazuaye <clement.bazuaye@interswitchgroup.com>
2025-02-13 16:51:06 +0100

    Refactor ToggleTheme and HomeScreenModule components for improved theme handling and UI responsiveness

--- components/toggle-mode.tsx
+++ components/toggle-mode.tsx
@@ -1,25 +1,51 @@
-import { useAppContext } from "@/context";
+import { useAppContext } from '@/context';
 
 import Feather from '@expo/vector-icons/Feather';
 
-import { useCallback, useState } from "react";
-import { Pressable, useColorScheme } from "react-native";
-import { useMediaAndPlatform } from "@/hooks";
+import { useCallback, useState } from 'react';
+import { Pressable, useColorScheme } from 'react-native';
+import { useMediaAndPlatform } from '@/hooks';
 const ToggleTheme = () => {
-  const colorTheme = useColorScheme();
-  const [ isDark, setIsDark ] = useState( colorTheme === "dark" );
-  console.log( 'isDark: ', isDark );
-  const { handleToggleTheme } = useAppContext();
-  const { widthScaleFactor, heightScaleFactor } = useMediaAndPlatform();
-  const iconSize = 24 * widthScaleFactor;
-  const handleToggle = useCallback( () => {
-    setIsDark( !isDark );
-    console.log( "Hello" );
-    handleToggleTheme( isDark ? "light" : "dark" );
-  }, [ handleToggleTheme, isDark ] );
-  return ( <Pressable style={ ( { pressed } ) => [ pressed && { opacity: 0.5, } ] } onPressIn={ handleToggle }>
-    { !isDark ? <Feather name="sun" size={ iconSize } color="black" /> : <Feather name="moon" size={ iconSize } color="white" /> }
-  </Pressable> );
+	const colorTheme = useColorScheme();
+	const [isDark, setIsDark] = useState(colorTheme === 'dark');
+	console.log('isDark: ', isDark);
+	const { handleToggleTheme } = useAppContext();
+	const { widthScaleFactor, heightScaleFactor } =
+		useMediaAndPlatform();
+	const iconSize = 24 * widthScaleFactor;
+	const handleToggle = useCallback(() => {
+		setIsDark(!isDark);
+		console.log('Hello');
+		handleToggleTheme(isDark ? 'light' : 'dark');
+	}, [handleToggleTheme, isDark]);
+	return (
+		<Pressable
+			style={({ pressed }) => [
+				pressed && { opacity: 0.5 },
+				{
+					backgroundColor: isDark
+						? 'hsla(220, 22%, 46%, .2)'
+						: 'hsla(220, 22%, 96%, 1)',
+					padding: 8 * widthScaleFactor,
+					borderRadius: 30,
+				},
+			]}
+			onPressIn={handleToggle}>
+			{!isDark ? (
+				<Feather
+					name="sun"
+					size={iconSize}
+					color="hsla(23, 11%, 10%, 1)"
+				/>
+			) : (
+				<Feather
+					name="moon"
+					size={iconSize}
+					color="white"
+				/>
+			)}
+		</Pressable>
+	);
 };
 
 export default ToggleTheme;
--- modules/home-screen.tsx
+++ modules/home-screen.tsx
@@ -4,108 +4,152 @@ import { fetchDX } from '@/lib/advance-fetch';
 import type { CountryType } from '@/types/countries';
 import { useQuery } from '@tanstack/react-query';
 import { Image } from 'expo-image';
-import { Link } from "expo-router";
+import { Link } from 'expo-router';
 import {
-  ActivityIndicator,
-  SafeAreaView,
-  SectionList,
-  Text,
-  View,
+	ActivityIndicator,
+	Pressable,
+	SafeAreaView,
+	SectionList,
+	View,
 } from 'react-native';
-
+import Feather from '@expo/vector-icons/Feather';
+import { useMediaAndPlatform } from '@/hooks';
 const HomeScreenModule = () => {
-  const { theme } = useAppContext();
-  const { data, isLoading, isError, error } = useQuery( {
-    queryKey: [ 'countries' ],
-    queryFn: async () => {
-      const data = await fetchDX<CountryType[]>( {
-        url: 'https://restcountries.com/v3.1/all',
-        options: {
-          method: 'GET',
-        },
-      } );
-      const arrageAlphabetically = data.reduce(
-        ( acc: { data: CountryType[]; type: string; }[], curre ) => {
-          const firstLetter = curre.name.common.charAt( 0 );
-          if ( !acc.find( ( item ) => item.type === firstLetter ) ) {
-            acc.push( {
-              type: firstLetter,
-              data: [ curre ],
-            } );
-          } else {
-            acc.find( ( item ) => item.type === firstLetter )?.data.push( curre );
-          }
-          return acc;
-        },
-        [],
-      );
-      console.log( 'arrageAlphabetically: ', arrageAlphabetically );
+	const { theme } = useAppContext();
+	const { widthScaleFactor } = useMediaAndPlatform();
+	const { data, isLoading, isError, error } = useQuery({
+		queryKey: ['countries'],
+		queryFn: async () => {
+			const data = await fetchDX<CountryType[]>({
+				url: 'https://restcountries.com/v3.1/all',
+				options: {
+					method: 'GET',
+				},
+			});
+			const arrageAlphabetically = data.reduce(
+				(acc: { data: CountryType[]; type: string }[], curre) => {
+					const firstLetter = curre.name.common.charAt(0);
+					if (!acc.find((item) => item.type === firstLetter)) {
+						acc.push({
+							type: firstLetter,
+							data: [curre],
+						});
+					} else {
+						acc.find((item) => item.type === firstLetter)?.data.push(curre);
+					}
+					return acc;
+				},
+				[],
+			);
+			console.log('arrageAlphabetically: ', arrageAlphabetically);
 
-      return (
-        arrageAlphabetically.sort( ( a, b ) =>
-          a.type.localeCompare( b.type ),
-        ) ?? []
-      );
-    },
-    enabled: true,
-  } );
-  // console.log( 'error: ', error );
+			return (
+				arrageAlphabetically.sort((a, b) =>
+					a.type.localeCompare(b.type),
+				) ?? []
+			);
+		},
+		enabled: true,
+	});
+	// console.log( 'error: ', error );
 
-  return (
-    <SafeAreaView>
-
-      { isLoading ? (
-        <ActivityIndicator
-          size={ 'large' }
-          color={ 'red' }
-        />
-      ) : (
-        data && (
-          <SectionList
-            stickySectionHeadersEnabled
-            ItemSeparatorComponent={ () => <View style={ { height: 16 } } /> }
-            contentContainerStyle={ { paddingInline: 24, paddingBottom: 24, backgroundColor: theme === 'dark' ? 'hsla(216, 99%, 7%, 1)' : 'white' } }
-            ListHeaderComponent={ <Text>Header</Text> }
-            renderSectionHeader={ ( { section } ) => (
-              <SectionHeader colorType={ theme }>
-
-                <UIText
-                  fontSize={ '16px' }
-                  colorType={ theme }>
-                  { section.type }
-                </UIText>
-              </SectionHeader>
-            ) }
-            sections={ data }
-            renderItem={ ( { item } ) => (
-              <Link href={ { pathname: "/country/[name]", params: { name: item.name.common } } }
-              >
-                <View style={ {
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  gap: 16,
-                } }>
-                  <View>
-                    <Image
-                      source={ `${ item.flags.png }` }
-                      style={ { width: 50, height: 50, aspectRatio: "1", objectFit: "fill", borderRadius: 50 } }
-                    />
-                  </View>
-                  <View>
-                    <UIText
-                      fontSize={ '16px' }
-                      colorType={ theme }>
-                      { item.name.common }
-                    </UIText>
-                  </View>
-                </View>
-              </Link>
-            ) }
-          />
-        )
-      ) }
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView>
+			{isLoading ? (
+				<ActivityIndicator
+					size={'large'}
+					color={'blue'}
+				/>
+			) : (
+				data && (
+					<SectionList
+						stickySectionHeadersEnabled
+						ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+						contentContainerStyle={{
+							paddingInline: 24,
+							paddingBottom: 24,
+							backgroundColor:
+								theme === 'dark' ? 'hsla(216, 99%, 7%, 1)' : 'white',
+						}}
+						ListHeaderComponent={
+							<View style={{ paddingTop: 20 }}>
+								<Pressable
+									style={({ pressed }) => [
+										{
+											padding: 16,
+											borderRadius: 4 * widthScaleFactor,
+											backgroundColor: 'hsla(220, 22%, 96%, 1)',
+											flexDirection: 'row',
+											justifyContent: 'center',
+										},
+										pressed && {
+											backgroundColor: pressed
+												? 'hsla(220, 22%, 96%, 1)'
+												: 'hsla(220, 22%, 84%, 1)',
+										},
+									]}>
+									<Feather
+										name="search"
+										size={24}
+										color="black"
+									/>
+									<UIText
+										colorType={theme}
+										fontSize="14px">
+										Search Country
+									</UIText>
+								</Pressable>
+							</View>
+						}
+						renderSectionHeader={({ section }) => (
+							<SectionHeader colorType={theme}>
+								<UIText
+									fontSize={'16px'}
+									colorType={theme}>
+									{section.type}
+								</UIText>
+							</SectionHeader>
+						)}
+						sections={data}
+						renderItem={({ item }) => (
+							<Link
+								href={{
+									pathname: '/country/[name]',
+									params: { name: item.name.common },
+								}}>
+								<View
+									style={{
+										flexDirection: 'row',
+										alignItems: 'center',
+										gap: 16,
+									}}>
+									<View>
+										<Image
+											source={`${item.flags.png}`}
+											style={{
+												width: 50,
+												height: 50,
+												aspectRatio: '1',
+												objectFit: 'fill',
+												borderRadius: 50,
+											}}
+										/>
+									</View>
+									<View>
+										<UIText
+											fontSize={'16px'}
+											colorType={theme}>
+											{item.name.common}
+										</UIText>
+									</View>
+								</View>
+							</Link>
+						)}
+					/>
+				)
+			)}
+		</SafeAreaView>
+	);
 };
 
 export default HomeScreenModule;
